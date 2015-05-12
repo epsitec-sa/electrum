63db6f29910b90f6af0ee28a35fd9cf90249b5f9
Pierre Arnaud <arnaud@epsitec.ch>
2015-05-12 06:17:01 +0200

    Add non-throwing checks for method/interface.

--- modules/check.js
+++ modules/check.js
@@ -2,12 +2,28 @@
 
 /*****************************************************************************/
 
+var hasMethod = function hasMethod (obj, method) {
+  return obj.hasOwnProperty (method) && typeof obj[method] === 'function';
+};
+
+/*****************************************************************************/
+
+var hasInterface = function hasInterface (obj, ...methods) {
+  for (var i = 0; i < methods.length; i++) {
+    if (!hasMethod (obj, methods[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/*****************************************************************************/
+
 var verifyMethod = function verifyMethod (obj, method, what) {
   what = what || 'interface';
   if (!obj.hasOwnProperty (method)) {
     throw 'The provided '+what+' does not implement method '+method+'.';
   }
-
   if (typeof (obj[method]) !== 'function') {
     throw 'The provided '+what+' contains '+method+', but it is not a function.';
   }
@@ -22,6 +38,8 @@ var verifyInterface = function verifyInterface (obj, ...methods) {
 /*****************************************************************************/
 
 module.exports = {
+  hasMethod: hasMethod,
+  hasInterface: hasInterface,
   verifyMethod: verifyMethod,
   verifyInterface: verifyInterface,
 };
