b1ee83bd1b83152e9445885f4b862965d5c5ee92
Pierre Arnaud <arnaud@epsitec.ch>
2015-12-08 09:33:03 +0100

    Test component getStates() called by EventHandlers.

--- src/test/electrum-bus.js
+++ src/test/electrum-bus.js
@@ -23,8 +23,8 @@ class CustomBus {
   dispatch (props, action) {
     this._log += `/dispatch id=${props.id} ${action}`;
   }
-  notify (props, value) {
-    this._log += `/notify id=${props.id} value=${value}`;
+  notify (props, value, ...states) {
+    this._log += `/notify id=${props.id} value="${value}" states=${JSON.stringify (states)}`;
   }
 }
 
@@ -41,18 +41,28 @@ class _Foo extends React.Component {
   getValue (target) {
     return target.checked ? 'on' : 'off';
   }
+  getStates () {
+    return [];
+  }
   render () {
     return <input id={this.props.id} type='checkbox' onChange={this.onChange} onFocus={this.onFocus}/>;
   }
 }
 
+class _Bar extends React.Component {
+  render () {
+    return <input id={this.props.id} type='text' onChange={this.onChange}/>;
+  }
+}
+
 const Foo = Electrum.wrap ('Foo', _Foo);
+const Bar = Electrum.wrap ('Bar', _Bar);
 
 /******************************************************************************/
 
 describe ('EventHandlers', () => {
   describe ('onChange event', () => {
-    it ('posts an event on the bus', () => {
+    it ('Foo/checkbox posts an event on the bus', () => {
       const component = renderIntoDocument (<Foo id='1' />);
       const input = findRenderedDOMComponentWithTag (component, 'input');
       expect (input).to.have.property ('type', 'checkbox');
@@ -60,29 +70,40 @@ describe ('EventHandlers', () => {
       bus.clearLog ();
       input.checked = true;
       Simulate.change (input);
-      expect (bus.log).to.equal ('/notify id=1 value=on');
+      expect (bus.log).to.equal ('/notify id=1 value="on" states=[]');
 
       bus.clearLog ();
       input.checked = false;
       Simulate.change (input);
-      expect (bus.log).to.equal ('/notify id=1 value=off');
+      expect (bus.log).to.equal ('/notify id=1 value="off" states=[]');
+    });
+
+    it ('Bar/text posts an event on the bus', () => {
+      const component = renderIntoDocument (<Bar id='1' />);
+      const input = findRenderedDOMComponentWithTag (component, 'input');
+      expect (input).to.have.property ('type', 'text');
+
+      bus.clearLog ();
+      input.value = 'John';
+      Simulate.change (input);
+      expect (bus.log).to.equal ('/notify id=1 value="John" states=[{"from":0,"to":0}]');
+
+      bus.clearLog ();
+      input.value = 'Jane';
+      Simulate.change (input);
+      expect (bus.log).to.equal ('/notify id=1 value="Jane" states=[{"from":0,"to":0}]');
     });
   });
 
   describe ('onFocus event', () => {
-    it ('posts an event on the bus', () => {
+    it ('Foo/checkbox posts an event on the bus', () => {
       const component = renderIntoDocument (<Foo id='1' />);
       const input = findRenderedDOMComponentWithTag (component, 'input');
       expect (input).to.have.property ('type', 'checkbox');
 
       bus.clearLog ();
-      Simulate.focus (input, {
-        stopPropagation: function () {
-          console.log ('stop');
-        }, preventDefault: function () {
-          console.log ('prevent');
-        }});
-      expect (bus.log).to.equal ('/notify id=1 value=off/dispatch id=1 focus');
+      Simulate.focus (input);
+      expect (bus.log).to.equal ('/notify id=1 value="off" states=[]/dispatch id=1 focus');
     });
   });
 });
